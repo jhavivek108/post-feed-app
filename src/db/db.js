2f175f0c176872f202e5275f8207a96c5d7b3fed
jhavivek108 <jhavivek2404@gmsail.com>
2026-05-04 23:57:35 +0530

    feat: add database and models

--- src/db/db.js
+++ src/db/db.js
@@ -2,4 +2,8 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
     await mongoose.connect("mongodb+srv://jhavivek6969_db_user:HiJnYC3y26iOJ4bD@cluster1.4psvilp.mongodb.net/project-1")
+
+    console.log("Connected to DB")
 }
+
+module.exports = connectDB;
