00b09fa355fe1ad8e8257de8c5e51ea9e66be995
jhavivek108 <jhavivek2404@gmail.com>
2026-05-11 17:00:48 +0000

    made connections

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const multer = require('multer');
 
 const app= express();
+//app.use()
+
+const upload = multer({
+    storage: multer.memoryStorage()
+})
+
+app.post('create-post', upload.single("image"), async (req,res) => {
+    console.log(req.body);
+})
 
 module.exports = app;
